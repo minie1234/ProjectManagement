fd6f012decdc5a09741b810995144bd67f9d8a5f
YunJin-Choi <azuer1989@naver.com>
2017-08-21 14:26:58 +0900

    AnalysisController 수정

---
+++ project-2/src/main/java/com/project/controller/AnalysisDataController.java
@@ -0,0 +1,13 @@
+package com.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.mapper.ProjectMapper;
+
+@RestController
+public class AnalysisDataController {
+	@Autowired
+	private ProjectMapper projectMapper;
+
+}
